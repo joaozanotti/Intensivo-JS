26824a5b52b70d41de1ff1043886d9c980fc256b
joaozanotti <joaovzanotti@gmail.com>
2024-03-21 23:09:24 -0300

    Criando objetos literais para salvar os tamanhos e link de imagens

---
+++ apple-watch-shop/objetos.js
@@ -0,0 +1,37 @@
+const imagensAzul = {
+    nome: "Azul-inverno",
+    pulseira: "imagens-azul-inverno/pulseira-azul.jpeg",
+    relogio: "imagens-azul-inverno/relogio-azul.jpeg",
+    tela: "imagens-azul-inverno/tela-azul.jpeg"
+}
+
+const imagensEstelar = {
+    nome: "Estelar",
+    pulseira: "imagens-estelar/pulseira-estelar.jpeg",
+    relogio: "imagens-estelar/relogio-estelar.jpeg",
+    tela: "imagens-estelar/tela-estelar.jpeg"
+}
+
+const imagensNoite = {
+    nome: "Meia-noite",
+    pulseira: "imagens-meia-noite/pulseira-noite.jpeg",
+    relogio: "imagens-meia-noite/relogio-noite.jpeg",
+    tela: "imagens-meia-noite/tela-noite.jpeg"
+}
+
+const imagensRosa = {
+    nome: "Rosa-claro",
+    pulseira: "imagens-rosa-claro/pulseira-rosa.jpeg",
+    relogio: "imagens-rosa-claro/relogio-rosa.jpeg",
+    tela: "imagens-rosa-claro/tela-rosa.jpeg"
+}
+
+const imagensVerde = {
+    nome: "Verde-cipreste",
+    pulseira: "imagens-verde-cipreste/pulseira-verde.jpeg",
+    relogio: "imagens-verde-cipreste/relogio-verde.jpeg",
+    tela: "imagens-verde-cipreste/tela-verde.jpeg"
+}
+
+export const opcoesTamanhos = ["41 mm", "45 mm"];
+export const opcoesCores = [imagensVerde, imagensAzul, imagensNoite, imagensEstelar, imagensRosa];
