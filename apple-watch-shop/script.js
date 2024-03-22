dcf0624948edfd9bdce1314ed681bbb63bd44f95
joaozanotti <joaovzanotti@gmail.com>
2024-03-21 23:11:30 -0300

    Criando as funcionalidades de alterar tamanho e alterar a cor dos itens

---
+++ apple-watch-shop/script.js
@@ -0,0 +1,37 @@
+import { opcoesTamanhos, opcoesCores } from "./objetos.js";
+
+const imagemVisualizacao = document.getElementById("imagem-visualizacao");
+const tituloProduto = document.getElementById("titulo-produto");
+const nomeCor = document.getElementById("nome-cor-selecionada");
+
+const botaoTamanho = document.querySelectorAll("[name='opcao-tamanho']");
+botaoTamanho.forEach(botao => botao.addEventListener("click", trocarTamanho));
+
+const botaoImagem = document.querySelectorAll("[name='opcao-imagem']");
+
+const botaoCor = document.querySelectorAll("[name='opcao-cor']");
+botaoCor.forEach(botao => botao.addEventListener("click", trocarCor));
+
+let tamanhoSelecionado = 1;
+let corSelecionada = 1;
+
+function trocarTamanho() {
+    let idBotao = this.id;
+    tamanhoSelecionado = idBotao.substring(8);
+
+    if (tamanhoSelecionado == 0) {
+        imagemVisualizacao.classList.add("caixa-pequena");
+    } else {
+        imagemVisualizacao.classList.remove("caixa-pequena");
+    }
+    tituloProduto.innerHTML = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome.toLowerCase() + " para caixa de " + opcoesTamanhos[tamanhoSelecionado];
+}
+
+function trocarCor() {
+    let idBotao = this.id;
+    corSelecionada = idBotao.substring(4);
+
+    imagemVisualizacao.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].relogio;
+    tituloProduto.innerHTML = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome.toLowerCase() + " para caixa de " + opcoesTamanhos[tamanhoSelecionado];
+    nomeCor.innerHTML = "Cor - " + opcoesCores[corSelecionada].nome;
+}
