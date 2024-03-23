54eac69841b86e6ae3e43989262ffc9304013cb7
joaozanotti <joaovzanotti@gmail.com>
2024-03-22 21:58:20 -0300

    Criando a função de alterar as imagens em miniatura de acordo com a cor

--- apple-watch-shop/script.js
+++ apple-watch-shop/script.js
@@ -1,6 +1,9 @@
 import { opcoesTamanhos, opcoesCores } from "./objetos.js";
 
 const imagemVisualizacao = document.getElementById("imagem-visualizacao");
+const imgMiniPulseira = document.getElementById("imagem-miniatura-0");
+const imgMiniRelogio = document.getElementById("imagem-miniatura-1");
+const imgMiniTela = document.getElementById("imagem-miniatura-2");
 const tituloProduto = document.getElementById("titulo-produto");
 const nomeCor = document.getElementById("nome-cor-selecionada");
 
@@ -8,16 +11,18 @@ const botaoTamanho = document.querySelectorAll("[name='opcao-tamanho']");
 botaoTamanho.forEach(botao => botao.addEventListener("click", trocarTamanho));
 
 const botaoImagem = document.querySelectorAll("[name='opcao-imagem']");
+botaoImagem.forEach(botao => botao.addEventListener("click", trocarImagem));
 
 const botaoCor = document.querySelectorAll("[name='opcao-cor']");
 botaoCor.forEach(botao => botao.addEventListener("click", trocarCor));
 
 let tamanhoSelecionado = 1;
 let corSelecionada = 1;
+let imagemSelecionada = 1;
 
 function trocarTamanho() {
     let idBotao = this.id;
-    tamanhoSelecionado = idBotao.substring(8);
+    tamanhoSelecionado = +idBotao.substring(8);
 
     if (tamanhoSelecionado == 0) {
         imagemVisualizacao.classList.add("caixa-pequena");
