b53cead28ebc39ab0828d1b82f0debdda63d81a9
joaozanotti <joaovzanotti@gmail.com>
2024-04-24 21:57:42 -0300

    Referenciando os elementos HTML e adicionando eventos para enviar e exibir mensagens

--- chat-tempo-real/public/interface.js
+++ chat-tempo-real/public/interface.js
@@ -1,7 +1,7 @@
-const socket = io();
 const chat = document.getElementById('mensagens');
 const btEnviar = document.getElementById('enviar');
 const elementoTexto = document.getElementById('texto');
+const socket = io();
 
 socket.addEventListener('nova mensagem', (msg) => {
     const mensagem = document.createElement('li');
